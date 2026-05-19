9f771595f540b882c41e3e69c98227f57cca8119
Nithin <nithinvskk@gmail.com>
2026-05-19 22:24:20 +0530

    Add files via upload

---
+++ NullPointerExceptionDemo.java
@@ -0,0 +1,13 @@
+package JavaOops;
+
+public class NullPointerExceptionDemo {
+    public static void main(String[] args) {
+        try {
+            String a = null;
+            System.out.println(a.charAt(0)); 
+        }
+        catch (NullPointerException e) {
+            System.out.println("Null Pointer Exception"); 
+        }
+    }
+}
