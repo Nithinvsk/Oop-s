b0c55ec2321a1656eb08051dd442afcfa4083d17
Nithin <nithinvskk@gmail.com>
2026-06-18 22:11:27 +0530

    Add files via upload

---
+++ StringBufferCapacity.java
@@ -0,0 +1,8 @@
+package JavaOops;
+
+public class StringBufferCapacity {
+	public static void main(String[] args) {
+		StringBuffer nt = new StringBuffer();
+		System.out.print(nt.capacity());
+	}
+}
