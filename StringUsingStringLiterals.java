85f0b99449fdcbb58a01724d0317a3faaaf58780
Nithin <nithinvskk@gmail.com>
2026-07-02 15:19:03 +0530

    Add files via upload

---
+++ StringUsingStringLiterals.java
@@ -0,0 +1,9 @@
+package JavaOops;
+
+public class StringUsingStringLiterals {
+	public static void main(String[] args) {
+		String s1 = "Nithin";
+		String s2 = "Nithin"; // two objects belongs to string pool
+		System.out.println(s1);
+	}
+}
