bb3cda333b01c408328618095807b43e994878e1
Nithin <nithinvskk@gmail.com>
2026-07-16 23:25:26 +0530

    Add files via upload

---
+++ TraceofMatrix2DArray.java
@@ -0,0 +1,31 @@
+package JavaOops;
+
+import java.util.Scanner;
+
+public class TraceofMatrix2DArray {
+	public static void main(String[] args) {
+        Scanner nt = new Scanner(System.in);
+		
+		System.out.print("Enter the Row : ");
+		int row = nt.nextInt();
+		
+		System.out.print("Enter the Column : ");
+		int column = nt.nextInt();
+		
+		int a[][] = new int[row][column],i,j,sum=0;
+		for (i=0;i<row;i++) {
+			for (j=0;j<column;j++) {
+				System.out.printf("Row %d Column %d : ",i+1,j+1);
+				a[i][j] = nt.nextInt();
+			}
+		}
+		for (i=0;i<row;i++) {
+			for (j=0;j<column;j++) {
+				if (i == j) {
+					sum += a[i][j];
+				}
+			}
+		}
+		System.out.println(sum);
+	}
+}
