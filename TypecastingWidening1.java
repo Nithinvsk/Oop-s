d04bc08f0f5066e9fb5d207b6b1cea20638747a2
Nithin <nithinvskk@gmail.com>
2026-07-22 09:33:10 +0530

    Add files via upload

---
+++ TypecastingWidening1.java
@@ -0,0 +1,10 @@
+package JavaOops;
+
+public class TypecastingWidening1 {
+	public static void main(String[] args) {
+		int num = 8;
+		double d = num;
+		System.out.println("Before Typecasting : "+num);
+		System.out.println("After Typecasting : "+d);
+	}
+}
